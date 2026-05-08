05b4a49a74ef2daa623c53f9c45dbf4d7b37b88a
Dapaaaaaa <daffaharahap191205@gmail.com>
2026-05-08 17:17:02 +0800

    Feat: Add html and setting to run this project

--- src/main/java/com/mlsystem/TierListApplication.java
+++ src/main/java/com/mlsystem/TierListApplication.java
@@ -1,4 +1,13 @@
 package com.mlsystem;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Ini "Sakti", fungsinya mengatur semua konfigurasi otomatis
 public class TierListApplication {
+
+    public static void main(String[] args) {
+        // Ini perintah untuk menyalakan mesin server
+        SpringApplication.run(TierListApplication.class, args);
+    }
 }
