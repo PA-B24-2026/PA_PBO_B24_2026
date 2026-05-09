58d8f840075d3d5466308b9d3e769a2c95f4e6d0
Dapaaaaaa <daffaharahap191205@gmail.com>
2026-05-09 14:48:46 +0800

    docs(index.html): Documentation for code

--- src/main/java/com/mlsystem/controller/HeroController.java
+++ src/main/java/com/mlsystem/controller/HeroController.java
@@ -1,15 +1,18 @@
 package com.mlsystem.controller;
 
+// Ini import dari java
+import java.util.List;
+
+// Ini import dari file kita
 import com.mlsystem.model.Hero;
 import com.mlsystem.service.HeroService;
+
+// Ini import ORG
 import org.apache.catalina.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
